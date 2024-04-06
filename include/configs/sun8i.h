5d98448e8a9d6a955389c76cc53fce5ea556dc3a
TURBOyan <turboisyan@foxmail.com>
2024-04-06 23:21:28 +0800

    [BUG]修复错误

--- include/configs/sun8i.h
+++ include/configs/sun8i.h
@@ -49,7 +49,7 @@
                             "load mmc 0:1 0x41800000 sun8i-v3s-licheepi-zero-dock.dtb; " \
                             "bootz 0x41000000 - 0x41800000;"
  
-#define CONFIG_BOOTARGS      "fadfadsfconsole=ttyS0,115200 panic=5 rootwait root=/dev/mmcblk0p2 earlyprintk rw  vt.global_cursor_default=0"
+#define CONFIG_BOOTARGS      "console=ttyS0,115200 panic=5 rootwait root=/dev/mmcblk0p2 earlyprintk rw  vt.global_cursor_default=0"
 
 
 /**
